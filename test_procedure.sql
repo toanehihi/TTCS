31ebb8aa91ee7b61246ebea0e5fc9650f9117643
toanehihi <hocongtoan987@gmail.com>
2025-06-05 23:12:09 +0700

    test case

--- test_procedure.sql
+++ test_procedure.sql
@@ -4,3 +4,18 @@ CALL InsertLecturerAccount('sieu1338','sieu1','sieu1@gmail.com','0331535722',1, 
 
 CALL InsertStudentAccount('sieu1123','sieu1123','0239715622','sieu1123@gmail.com',1, '1999-01-01', 1);
 
+CALL InsertSemester('2026-1','Học kì 1 2025-2026','2025-08-04','2025-12-31','1');
+
+CALL InsertSchedule(1,1,2,2,84,1,4,61,'PRACTICE');
+
+CALL GetAllStudentsByClassId(2);
+
+CALL GetAllStudentsByCourseSection(2);
+
+CALL GetAllStudentsByCourseId(1);
+
+CALL GetLecturerReports(83);
+
+CALl GetAllReports();
+
+CALL GetAllSchedulesByCourseId(1);
